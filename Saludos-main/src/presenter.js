1ed06089d09dc980bb4ed053cacc12f241318f6a
Jherson <Jherson.chambi@ucb.edu.bo>
2024-08-18 22:42:27 +0200

    Segunda Version ( el saludo y el genero)

--- Saludos-main/src/presenter.js
+++ Saludos-main/src/presenter.js
@@ -1,8 +1,18 @@
 const button = document.querySelector("#saludar-button");
 const nombreInput = document.querySelector("#nombre");
+const generoSelect = document.querySelector("#genero");
 const div = document.querySelector("#resultado-div");
 
 button.addEventListener("click", () => {
   const nombre = nombreInput.value;
-  div.innerHTML = `<p>Hola..querido usuario, ${nombre}</p>`;
+  const genero = generoSelect.value;
+  let saludo = `Hola...querid@, ${nombre}`;
+
+  if (genero === "masculino") {
+    saludo = `Hola, Señor ${nombre}`;
+  } else if (genero === "femenino") {
+    saludo = `Hola, Señora ${nombre}`;
+  }
+
+  div.innerHTML = `<p>${saludo}</p>`;
 });
